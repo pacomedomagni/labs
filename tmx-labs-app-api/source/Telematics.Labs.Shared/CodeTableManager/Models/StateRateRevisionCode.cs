1b76ea511bf3b2087e3c81fff1284a2bbecc0272
Kouakou Domagni <kouakou_p_domagni@progressive.com>
2026-02-26 10:30:27 -0600

    major changes

--- tmx-labs-app-api/source/Telematics.Labs.Shared/CodeTableManager/Models/StateRateRevisionCode.cs
+++
@@ -1,7 +0,0 @@
-namespace FulfillmentWeb.Shared.CodeTableManager.Models;
-
-public class StateRateRevisionCode
-{
-    public string State { get; set; }
-    public string RateRevisionCode { get; set; }
-}
